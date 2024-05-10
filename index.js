abc16812742835984a1b74f7756fbf8eaf132765
git12nazneen <lipinazneen65@gmail.com>
2024-05-10 23:12:57 +0600

    added single data

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -38,6 +38,12 @@ async function run() {
         res.send(result);
     })
 
+    app.get('/room/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query= {_id : new ObjectId(id)}
+      const result = await addHubCollection.findOne(query)
+      res.send(result)
+    })
 
 
     await client.db("admin").command({ ping: 1 });
